438727ea25120cf9e0aa16a3ae4cf89a807e6228
Brandon Wright <brandonfullstack@gmail.com>
2023-06-25 19:50:36 -0400

    WIP

--- js/scripts.js
+++ js/scripts.js
@@ -10,13 +10,34 @@ function isEmpty() {
   return false;
 }
 
+function isVowelOrConsonant() {
+  ignoredCharacters != ["a", "b", "c", "d"]
+}
+
 // Business Logic
 
 function pigLatin(text) {
   if (isEmpty(text)) {
     return "Error: please enter text.";
   }
+  else if (isVowelOrConsonant.includes(text)) {
+    return text;
+  }
 }
 
+function omitWords(text) {
+  const omittedWords = ["zoinks", "muppeteer", "biffaroni", "loopdaloop"];
+  const words = text.split(' ');
+
+  words.forEach(function(word, index) {
+      if (omittedWords.includes(word.toLowerCase())) {
+      words[index] = '***'; // Replace the word with asterisks
+      }
+  });
+
+  let omittedText = words.join(' ');
+
+  return omittedText;
+  }
 
 // UI Logic
