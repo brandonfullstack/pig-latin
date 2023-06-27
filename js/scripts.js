efd944fc6658f8dcb0322561a9cc0285fff07fc9
Brandon Wright <brandonfullstack@gmail.com>
2023-06-26 22:27:16 -0400

    WIP: add logic and UI

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,35 @@
-// Utility Logic
-
-function isEmpty() {
-  for (let i=0; i < arguments.length; i++) {
-    console.log(arguments[i]);
-    if (arguments[i].trim().length === 0) {
-      return true;
-    }
-  }
-  return false;
-}
-
-function isVowelOrConsonant() {
-  ignoredCharacters != ["a", "b", "c", "d"]
-}
-
 // Business Logic
 
-function pigLatin(text) {
-  if (isEmpty(text)) {
-    return "Error: please enter text.";
+function pigLatin(word) {
+  let pLWord;
+  const textArray = word.split("");
+  for(let i = 0; i <= word.length; i += 1) {
+    if (textArray[0] === "a" || textArray[0] === "e" || textArray[0] === "i" || textArray[0] === "o" || textArray[0] === "u") {
+      pLWord = (word + "way");
+    } else if (textArray[0] === "q" ) {
+      pLWord = (word.slice(2) + word.slice(0,2) + "ay");
+    } else if (textArray[0] === "s" && textArray[1] === "q" && textArray[2] === "u") {
+      pLWord = (word.slice(3) + word.slice(0,3) + "ay");
+    } else if (textArray[2] !== "a" || textArray[2] !== "e" || textArray[2]!=="i" || textArray[2] === "o" || textArray[2] !== "u") { 
+        pLWord = (word.slice(3) + word.slice(0,3) + "ay")
+    } else if (textArray[1] !== "a" || textArray[1] !== "e" || textArray[1] !== "i" || textArray[1] !== "o" || textArray[1] !== "u") {
+        pLWord = (word.slice(2) + word.slice(0,2) + "ay")
+    } else {
+      pLWord = (word.slice(1) + word.slice(0,1) + "ay")
+    }
   }
-  else if (isVowelOrConsonant.includes(text)) {
-    return text;
-  }
-}
-
-function omitWords(text) {
-  const omittedWords = ["zoinks", "muppeteer", "biffaroni", "loopdaloop"];
-  const words = text.split(' ');
-
-  words.forEach(function(word, index) {
-      if (omittedWords.includes(word.toLowerCase())) {
-      words[index] = '***'; // Replace the word with asterisks
-      }
-  });
-
-  let omittedText = words.join(' ');
-
-  return omittedText;
-  }
+  return pLWord;
+};
 
 // UI Logic
+
+function handleFormSubmission() {
+  event.preventDefault();
+  const passage = document.getElementById("text-passage").value;
+  const pLWord = pigLatin(passage);
+  document.getElementById("tranlated-text").innerText = pLWord;
+}
+
+window.addEventListener("load", function() {
+  document.querySelector("form#text-translator").addEventListener("submit", handleFormSubmission);
+});
